e3f3c5b52f65070467864ee6c983d6f3d0628428
FIRST_NAME LAST_NAME <60664170+henry970@users.noreply.github.com>
2022-10-09 00:46:25 +0100

    update file

---
+++ main.py
@@ -0,0 +1,99 @@
+#Task 1---------------------------------
+
+print("Hello World")
+print("Okolie Chijioke Henry")
+
+#Task 2------------------------------------
+
+
+
+my_var = 12.6
+print(my_var)
+
+number = 200
+print(number)
+
+my_name = "Okolie Chijioke Henry"
+print(my_name)
+
+
+#Task 3---------------------------------------
+
+# Declaring a raw string #Task 3
+
+name = r"I am Okolie\n Henry\n\tChijioke\b"
+print(name)
+
+first_name = "Henry"
+surname = "Okolie"
+add_name = first_name + ' ' + surname
+print(add_name)
+
+#Task 4--------------------------------------
+
+number1 = 54
+number2 = 50
+add_value = number1 + number2
+print(add_value)
+
+number1 = 54
+number2 = 50
+add_value = number1 - number2
+print(add_value)
+
+number1 = 54
+number2 = 50
+add_value = number1 / number2
+print(add_value)
+
+#Task 5---------------------------------------
+
+number = 3
+if number == 2:
+    print("Number is Two")
+else:
+    print("Number is not Two")
+
+
+#Task 6--------------------------------------
+
+number1 = 70
+number2 = 75
+
+if number1 > 50 and number2 < 100:
+    print("Evaluation is True")
+
+
+if number1 > 50 and number2 < 100:
+    print("Evaluation is True")
+else:
+    print("Evaluation is False")
+
+
+name = "python"
+if name == "Testify":
+    print("Yay!! The string is a Testify Python")
+
+#Task 7---------------------------------------
+
+number = 11
+
+for i in range(number):
+    print("Hello World", i)
+
+
+
+#Task 8---------------------------------------
+
+
+number = 10
+for i in range(number):
+    if i == 2:
+        continue
+    print("for: number:", i)
+
+for i in range(number):
+    if i == 8:
+        break
+    print("Number:", i)
+
